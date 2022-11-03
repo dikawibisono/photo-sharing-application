abf2731a4394e8cc6f4e01675678ac433a21e60d
dikasono <97390732+dikawibisono@users.noreply.github.com>
2022-11-03 20:45:47 +0700

    fix jwt

--- controller/auth.js
+++ controller/auth.js
@@ -1,34 +1,16 @@
 const jwt = require('jsonwebtoken');
-const db = require('../database/dbConnection');
 require('dotenv').config()
 
-
-
 const verifyAcc = (req,res, next) =>{
-    token = req.header('Bearer-token');
-
-    if(!token){return res.status(401).json({message: "Access token is required, please login to get access token"})}
-
-    try{
-        const {username, password} = jwt.verify(token, process.env.SECRET_TOKEN);
-
-        db.query(`SELECT * FROM users where username = "${username}"`, (err, result) => {
-            if (err) throw err
     
-            if (result.length == 0) {return res.status(400).json({
-                message: `Please register first`
-            })}
-    
-            if (result[0].password !== password) { return res.status(400).json({
-                message: `Invalid token`
-            })}
-        })
-
-        next()
-    } catch (error){
-        res.status(400).json({message: "Invalid token"})
+    try {
+    jwt.verify(req.header('authorization').split(' ')[1], process.env.SECRET_TOKEN)
+    next()
+    } catch (e) {
+    res.status(401).json({
+        message: "unauthorized, please login!"
+    })
     }
-
 }
 
 module.exports = verifyAcc
