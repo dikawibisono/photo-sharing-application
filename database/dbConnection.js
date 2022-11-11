f3f55ec4b5bef89e8d806a5f363276def01d8f20
dikasono <97390732+dikawibisono@users.noreply.github.com>
2022-11-11 23:31:16 +0700

    deploy flyio

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'photo_sharing_application'
-})
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'photo_sharing_application'
+// })
+
+
+const db = mysql.createConnection(process.env.DATABASE_URL)
+
+
 
 module.exports = db
